7c339e7863789a7e950d6328edf7317be891b64b
Afif Zafri <zafri_21596@yahoo.com>
2018-09-12 22:56:10 +0800

    added delay before start new activity to show splash screen

--- app/src/main/java/com/afifzafri/backpacktrack/SplashActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/SplashActivity.java
@@ -3,6 +3,7 @@ package com.afifzafri.backpacktrack;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -21,13 +22,25 @@ public class SplashActivity extends AppCompatActivity {
         // if token not exist, means not login so redirect to login page
         if(access_token == null)
         {
-            Intent intentPage = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(intentPage);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intentPage = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intentPage);
+                    finish();
+                }
+            }, 2000);
         }
         else
         {
-            Intent intentPage = new Intent(SplashActivity.this, HomeActivity.class);
-            startActivity(intentPage);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intentPage = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(intentPage);
+                    finish();
+                }
+            }, 2000);
         }
     }
 }
