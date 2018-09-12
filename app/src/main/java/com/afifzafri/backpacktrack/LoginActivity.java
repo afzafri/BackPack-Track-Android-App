703169374ff7bbd2fd8860ffffb3cfcda888decc
Afif Zafri <zafri_21596@yahoo.com>
2018-09-12 15:21:43 +0800

    test receive login input

--- app/src/main/java/com/afifzafri/backpacktrack/LoginActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/LoginActivity.java
@@ -2,6 +2,11 @@ package com.afifzafri.backpacktrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginBut = (Button) findViewById(R.id.loginBut);
+
+        loginBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username = (EditText) findViewById(R.id.username);
+                EditText password = (EditText) findViewById(R.id.password);
+                CheckBox remember_me = (CheckBox) findViewById(R.id.remember_me);
+
+                Toast.makeText(getApplicationContext(), "Username: "+ username.getText().toString() + " Password: "+ password.getText().toString() + " Remember me: "+ (remember_me.isChecked() ? 1 : 0), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
