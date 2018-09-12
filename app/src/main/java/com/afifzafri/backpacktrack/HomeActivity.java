78a536cc86de884e3123e5244fbb598c3adad680
Afif Zafri <zafri_21596@yahoo.com>
2018-09-12 23:07:59 +0800

    add testing logout button and method

--- app/src/main/java/com/afifzafri/backpacktrack/HomeActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,10 +16,18 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         // read from SharedPreferences
-        SharedPreferences sharedpreferences = getSharedPreferences("logindata", Context.MODE_PRIVATE);
+        final SharedPreferences sharedpreferences = getSharedPreferences("logindata", Context.MODE_PRIVATE);
         String name = sharedpreferences.getString("name", "");
 
         TextView user_name = (TextView) findViewById(R.id.user_name);
         user_name.setText(name);
+
+        Button logoutBut = (Button) findViewById(R.id.logoutBut);
+        logoutBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences.edit().clear().commit();
+            }
+        });
     }
 }
