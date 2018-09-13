0c9781746a28a428ee9670fbdf1f3bd5c675eac6
Afif Zafri <zafri_21596@yahoo.com>
2018-09-14 00:32:50 +0800

    get all input from form

--- app/src/main/java/com/afifzafri/backpacktrack/RegisterActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.afifzafri.backpacktrack;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -81,6 +84,65 @@ public class RegisterActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queueCountries.add(countriesListRequest);
+
+        // Register button clicked
+        registerBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create dialog box, ask confirmation before proceed
+                AlertDialog.Builder alert = new AlertDialog.Builder(RegisterActivity.this);
+                alert.setTitle("Register");
+                alert.setMessage("Are you sure you want to register your account?");
+                // set positive button, yes etc
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+                        
+                        // get all input
+                        EditText nameIn = (EditText) findViewById(R.id.name);
+                        EditText usernameIn = (EditText) findViewById(R.id.username);
+                        EditText phoneIn = (EditText) findViewById(R.id.phone);
+                        EditText addressIn = (EditText) findViewById(R.id.address);
+                        Spinner countrySpinner = (Spinner) findViewById(R.id.country);
+                        EditText emailIn = (EditText) findViewById(R.id.email);
+                        EditText passwordIn = (EditText) findViewById(R.id.password);
+                        EditText password_confirmationIn = (EditText) findViewById(R.id.password_confirmation);
+
+                        String name = nameIn.getText().toString();
+                        String username = usernameIn.getText().toString();
+                        String phone = phoneIn.getText().toString();
+                        String address = addressIn.getText().toString();
+                        StringWithTag country = (StringWithTag) countrySpinner.getSelectedItem();
+                        String country_id = (String) country.key;
+                        String email = emailIn.getText().toString();
+                        String password = passwordIn.getText().toString();
+                        String password_confirmation = password_confirmationIn.getText().toString();
+
+                        if(name != null && username != null && phone != null && address != null
+                                && country_id != null && email != null && password != null && password_confirmation != null)
+                        {
+                            Toast.makeText(getApplicationContext(), "Register Success!", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Please fill in all the input!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                // set negative button, no etc
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                alert.show(); // show alert message
+            }
+        });
 
         // if click login, show login page
         loginBut.setOnClickListener(new View.OnClickListener() {
