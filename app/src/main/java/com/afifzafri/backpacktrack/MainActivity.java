acc55e67ba5b426ea9fb6c0ca77c41a834d05dfc
Afif Zafri <zafri_21596@yahoo.com>
2018-09-15 23:25:14 +0800

    created Home Fragment, and load the fragment into main activity

--- app/src/main/java/com/afifzafri/backpacktrack/MainActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/MainActivity.java
@@ -3,25 +3,39 @@ package com.afifzafri.backpacktrack;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        Fragment fragment = null;
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    fragment = new HomeFragment();
+                    setTitle(getString(R.string.title_home));
+                    break;
+                case R.id.navigation_itineraries:
+                    fragment = new HomeFragment();
+                    setTitle(getString(R.string.title_itineraries));
+                    break;
+                case R.id.navigation_create:
+                    fragment = new HomeFragment();
+                    setTitle(getString(R.string.title_create));
+                    break;
+                case R.id.navigation_profile:
+                    fragment = new HomeFragment();
+                    setTitle(getString(R.string.title_profile));
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
     };
 
@@ -30,9 +44,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+        setTitle(getString(R.string.title_home));
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
 }
