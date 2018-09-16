16ab298e75f879a37e6b68ad1d9dcdca26ebe386
Afif Zafri <zafri_21596@yahoo.com>
2018-09-16 17:18:53 +0800

    append all data into form

--- app/src/main/java/com/afifzafri/backpacktrack/EditProfileActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/EditProfileActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -14,14 +17,18 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EditProfileActivity extends AppCompatActivity {
@@ -33,10 +40,67 @@ public class EditProfileActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back navigation
 
+        // get UI elements
+        final EditText editName = (EditText) findViewById(R.id.editName);
+        final EditText editUsername = (EditText) findViewById(R.id.editUsername);
+        final EditText editPhone = (EditText) findViewById(R.id.editPhone);
+        final EditText editAddress = (EditText) findViewById(R.id.editAddress);
+        final EditText editEmail = (EditText) findViewById(R.id.editEmail);
+        final Spinner countryspinner = (Spinner) findViewById(R.id.countryspinner);
         final FrameLayout loadingFrame = (FrameLayout) findViewById(R.id.loadingFrame);
+        final List<String> countrieslist2 = new ArrayList<String>(); // need 2nd array, for getting position of country
 
         // show loading spinner
         loadingFrame.setVisibility(View.VISIBLE);
+
+        // Populate Countries spinner
+        // Instantiate the RequestQueue.
+        RequestQueue queueCountries = Volley.newRequestQueue(getApplicationContext());
+
+        // Request a string response from the provided URL.
+        JsonArrayRequest countriesListRequest = new JsonArrayRequest(Request.Method.GET, AppConstants.baseurl + "/api/listCountries", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        // Countries Array
+                        ArrayList<StringWithTag> countrieslist = new ArrayList<StringWithTag>();
+                        countrieslist.add(new StringWithTag(null, "Select countries...")); // set default first element in the spinner
+                        countrieslist2.add("Select countries..."); // also insert into 2nd list
+
+                        for(int i=0;i<response.length();i++)
+                        {
+                            try {
+                                JSONObject country = response.getJSONObject(i);
+                                String id = country.getString("id");
+                                String name = country.getString("name");
+
+                                countrieslist.add(new StringWithTag(id, name));
+                                countrieslist2.add(name);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        // Populate the spinner with Array values
+                        ArrayAdapter<StringWithTag> countriesAdapter = new ArrayAdapter<StringWithTag>(getApplicationContext(),   android.R.layout.simple_spinner_item, countrieslist);
+                        countriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                        countryspinner.setAdapter(countriesAdapter);
+
+                        Toast.makeText(getApplicationContext(), "Load Countries Success!", Toast.LENGTH_SHORT).show();
+                        loadingFrame.setVisibility(View.GONE);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Load Countries Failed!", Toast.LENGTH_SHORT).show();
+                loadingFrame.setVisibility(View.GONE);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queueCountries.add(countriesListRequest);
 
         // read from SharedPreferences
         final SharedPreferences sharedpreferences = getSharedPreferences("logindata", Context.MODE_PRIVATE);
@@ -52,8 +116,39 @@ public class EditProfileActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        Toast.makeText(getApplicationContext(), "Profile data loaded!"+response.toString(), Toast.LENGTH_SHORT).show();
-                        loadingFrame.setVisibility(View.GONE); // hide loading spinner
+                        try {
+
+                            // parse JSON response
+                            String name = response.getString("name");
+                            String username = response.getString("username");
+                            String phone = response.getString("phone");
+                            String address = response.getString("address");
+                            String email = response.getString("email");
+                            String avatar_url = response.getString("avatar_url");
+                            JSONObject country = response.getJSONObject("country");
+                            String country_name = country.getString("name");
+                            String country_id = country.getString("id");
+
+                            // set values to the elements
+                            editName.setText(name);
+                            editUsername.setText(username);
+                            editPhone.setText(phone);
+                            editAddress.setText(address);
+                            editEmail.setText(email);
+
+                            // set spinner selection to match current country
+                            if (country_name != null && !country_name.isEmpty() && country_name != "null") {
+                                int pos = countrieslist2.indexOf(country_name);
+                                countryspinner.setSelection(pos);
+                            }
+
+                            Toast.makeText(getApplicationContext(), "Profile data loaded!", Toast.LENGTH_SHORT).show();
+                            loadingFrame.setVisibility(View.GONE); // hide loading spinner
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            loadingFrame.setVisibility(View.GONE);
+                        }
 
                     }
                 }, new Response.ErrorListener() {
