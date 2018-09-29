72472c3fccc8117db1fa75e173931da87c1a9aa8
Afif Zafri <zafri_21596@yahoo.com>
2018-09-29 16:09:23 +0800

    display total budget and trip duration

--- app/src/main/java/com/afifzafri/backpacktrack/ListItinerariesAdapter.java
+++ app/src/main/java/com/afifzafri/backpacktrack/ListItinerariesAdapter.java
@@ -20,6 +20,8 @@ public class ListItinerariesAdapter extends RecyclerView.Adapter<ListItineraries
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView itinerary_title;
+        public TextView itinerary_duration;
+        public TextView itinerary_totalbudget;
         public TextView itinerary_user;
         public CardView mCardView;
         public ItinerariesModel currentItem;
@@ -28,6 +30,12 @@ public class ListItinerariesAdapter extends RecyclerView.Adapter<ListItineraries
             super(v);
             itinerary_title = (TextView) v.findViewById(R.id.itinerary_title);
             this.itinerary_title = itinerary_title;
+
+            itinerary_duration = (TextView) v.findViewById(R.id.itinerary_duration);
+            this.itinerary_duration = itinerary_duration;
+
+            itinerary_totalbudget = (TextView) v.findViewById(R.id.itinerary_totalbudget);
+            this.itinerary_totalbudget = itinerary_totalbudget;
 
             itinerary_user = (TextView) v.findViewById(R.id.itinerary_user);
             this.itinerary_user = itinerary_user;
@@ -70,8 +78,14 @@ public class ListItinerariesAdapter extends RecyclerView.Adapter<ListItineraries
         // Set itinerary title
         holder.itinerary_title.setText(itinerariesList.get(position).getTitle());
 
+        // Set itinerary duration
+        holder.itinerary_duration.setText(itinerariesList.get(position).getDuration());
+
+        // Set itinerary total budget
+        holder.itinerary_totalbudget.setText(itinerariesList.get(position).getTotalBudget());
+
         // Set itinerary user name
-        holder.itinerary_user.setText(itinerariesList.get(position).getUser());
+        holder.itinerary_user.setText("by " + itinerariesList.get(position).getUser());
 
         // get current position item data
         holder.currentItem = itinerariesList.get(position);
