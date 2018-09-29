10ff08de44c6c78dfe34d92b57982dbd94fe0958
Afif Zafri <zafri_21596@yahoo.com>
2018-09-29 14:47:04 +0800

    implement onClickListener to the recyclerview countries list

--- app/src/main/java/com/afifzafri/backpacktrack/ListCountriesAdapter.java
+++ app/src/main/java/com/afifzafri/backpacktrack/ListCountriesAdapter.java
@@ -1,11 +1,14 @@
 package com.afifzafri.backpacktrack;
 
+import android.content.ClipData;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -23,6 +26,9 @@ public class ListCountriesAdapter extends RecyclerView.Adapter<ListCountriesAdap
         // each data item is just a string in this case
         public TextView mTextView;
         public ImageView mImageView;
+        public CardView mCardView;
+        public VisitedCountriesModel currentItem;
+
         public MyViewHolder(View v) {
             super(v);
             mTextView = (TextView) v.findViewById(R.id.country_name);
@@ -30,6 +36,17 @@ public class ListCountriesAdapter extends RecyclerView.Adapter<ListCountriesAdap
 
             mImageView = (ImageView) v.findViewById(R.id.country_flag);
             this.mImageView = mImageView;
+
+            mCardView = (CardView) v.findViewById(R.id.country_card);
+            this.mCardView = mCardView;
+
+            // when card is clicked, get id
+            mCardView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    // item clicked
+                    Toast.makeText(v.getContext(), currentItem.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
@@ -64,6 +81,9 @@ public class ListCountriesAdapter extends RecyclerView.Adapter<ListCountriesAdap
 
         // Hide country id into image
         holder.mImageView.setTag(countriesList.get(position).getId());
+
+        // get current position item data
+        holder.currentItem = countriesList.get(position);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
