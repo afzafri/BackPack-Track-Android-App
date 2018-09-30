684e76f966dc3bf892ebfc8f91dcb6b2eb2392d6
Afif Zafri <zafri_21596@yahoo.com>
2018-10-01 02:14:48 +0800

    display the article summary

--- app/src/main/java/com/afifzafri/backpacktrack/ViewArticleActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/ViewArticleActivity.java
@@ -46,6 +46,7 @@ public class ViewArticleActivity extends AppCompatActivity {
         final TextView article_title = (TextView) findViewById(R.id.article_title);
         final TextView article_author = (TextView) findViewById(R.id.article_author);
         final TextView article_date = (TextView) findViewById(R.id.article_date);
+        final TextView article_summary = (TextView) findViewById(R.id.article_summary);
         final WebView webview_content = (WebView) findViewById(R.id.webview_content);
         final FrameLayout loadingFrame = (FrameLayout) findViewById(R.id.loadingFrame);
 
@@ -65,12 +66,14 @@ public class ViewArticleActivity extends AppCompatActivity {
                             String title = response.getString("title");
                             String author = response.getString("author");
                             String date = response.getString("date");
+                            String summary = response.getString("summary");
                             String content = response.getString("content");
 
                             // set values to the elements
                             article_title.setText(title);
                             article_author.setText(author);
                             article_date.setText(date);
+                            article_summary.setText(summary);
                             // to display HTML content, use webview
                             webview_content.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
 
