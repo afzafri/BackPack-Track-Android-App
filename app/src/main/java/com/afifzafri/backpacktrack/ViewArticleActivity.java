90630292bcfcfe6e9aab897f4a0a500c0ffbce73
Afif Zafri <zafri_21596@yahoo.com>
2018-10-01 01:46:52 +0800

    load article from API

--- app/src/main/java/com/afifzafri/backpacktrack/ViewArticleActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/ViewArticleActivity.java
@@ -1,8 +1,28 @@
 package com.afifzafri.backpacktrack;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewArticleActivity extends AppCompatActivity {
 
@@ -12,6 +32,77 @@ public class ViewArticleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_article);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back navigation
+
+        // get data pass through intent
+        Bundle extras = getIntent().getExtras();
+        final String article_id = extras.getString("article_id");
+
+        // read from SharedPreferences
+        final SharedPreferences sharedpreferences = getSharedPreferences("logindata", Context.MODE_PRIVATE);
+        final String access_token = sharedpreferences.getString("access_token", "");
+
+        // get UI elements
+        final TextView article_title = (TextView) findViewById(R.id.article_title);
+        final TextView article_author = (TextView) findViewById(R.id.article_author);
+        final TextView article_date = (TextView) findViewById(R.id.article_date);
+        final TextView article_content = (TextView) findViewById(R.id.article_content);
+        final FrameLayout loadingFrame = (FrameLayout) findViewById(R.id.loadingFrame);
+
+        // ----- Fetch article data and display -----
+        // Request a string response from the provided URL.
+        JsonObjectRequest articleRequest = new JsonObjectRequest(Request.Method.GET, AppHelper.baseurl + "/api/viewArticle/" + article_id, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+                            // parse JSON response
+                            String title = response.getString("title");
+                            String author = response.getString("author");
+                            String date = response.getString("date");
+                            String content = response.getString("content");
+
+                            // set values to the elements
+                            article_title.setText(title);
+                            article_author.setText(author);
+                            article_date.setText(date);
+
+                            // display HTML
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                                article_content.setText(Html.fromHtml(content,Html.FROM_HTML_MODE_LEGACY));
+                            } else {
+                                article_content.setText(Html.fromHtml(content));
+                            }
+
+                            Toast.makeText(getApplicationContext(), "Article loaded!", Toast.LENGTH_SHORT).show();
+                            loadingFrame.setVisibility(View.GONE); // hide loading spinner
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            loadingFrame.setVisibility(View.GONE);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Article not loaded! Please check your connection.", Toast.LENGTH_SHORT).show();
+                loadingFrame.setVisibility(View.GONE);
+            }
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer "+access_token);
+
+                return params;
+            }
+        };
+
+        // Add the request to the VolleySingleton.
+        VolleySingleton.getInstance(getBaseContext()).addToRequestQueue(articleRequest);
 
     }
 
