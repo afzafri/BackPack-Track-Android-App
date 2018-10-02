aede5442c702fe47cd8a216b08f8d2125538d8d6
Afif Zafri <zafri_21596@yahoo.com>
2018-10-02 23:10:40 +0800

    add back navigation to ad activities activity

--- app/src/main/java/com/afifzafri/backpacktrack/MyActivitiesActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/MyActivitiesActivity.java
@@ -2,6 +2,7 @@ package com.afifzafri.backpacktrack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MyActivitiesActivity extends AppCompatActivity {
 
@@ -10,10 +11,28 @@ public class MyActivitiesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_activities);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back navigation
+
         // get data pass through intent
         Bundle extras = getIntent().getExtras();
         final String itinerary_id = extras.getString("itinerary_id");
         String itinerary_title = extras.getString("itinerary_title");
         setTitle("Add activities for " + itinerary_title);
+    }
+
+    // override default back navigation action
+    // need finish(), to destroy the current activity so that it go back to last activity with last fragment
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // todo: goto back activity from here
+
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
