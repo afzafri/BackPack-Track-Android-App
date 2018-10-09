61eb085c8fe24fa4acf76d347ab953709d04672a
Afif Zafri <zafri_21596@yahoo.com>
2018-10-10 02:05:20 +0800

    implement addOnTabSelectedListener, so that change fragment when tab is clicked

--- app/src/main/java/com/afifzafri/backpacktrack/ViewItineraryActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/ViewItineraryActivity.java
@@ -1,10 +1,14 @@
 package com.afifzafri.backpacktrack;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class ViewItineraryActivity extends AppCompatActivity {
 
@@ -49,6 +53,23 @@ public class ViewItineraryActivity extends AppCompatActivity {
         simpleViewPager.setAdapter(adapter);
         // addOnPageChangeListener event change the tab on slide
         simpleViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                simpleViewPager.setCurrentItem(tab.getPosition());
+                //Log.i("TAG", "onTabSelected: " + tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //Log.i("TAG", "onTabUnselected: " + tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                //Log.i("TAG", "onTabReselected: " + tab.getPosition());
+            }
+        });
 
     }
 
