4c6665be39f50794e3f54e0fe333370a98473e18
Afif Zafri <zafri_21596@yahoo.com>
2018-10-10 03:47:27 +0800

    load list of coordinates from API, and add marker to the google map

--- app/src/main/java/com/afifzafri/backpacktrack/MapFragment.java
+++ app/src/main/java/com/afifzafri/backpacktrack/MapFragment.java
@@ -12,7 +12,15 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -22,15 +30,24 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
 
+    // initialize variables for google map
     MapView mMapView;
     private GoogleMap googleMap;
-
 
     public MapFragment() {
         // Required empty public constructor
@@ -41,15 +58,16 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        final View view = inflater.inflate(R.layout.fragment_map, container, false);
 
         // get itinerary id
-        String itinerary_id = getArguments().getString("itinerary_id");
+        final String itinerary_id = getArguments().getString("itinerary_id");
 
         // read from SharedPreferences
         final SharedPreferences sharedpreferences = getActivity().getSharedPreferences("logindata", Context.MODE_PRIVATE);
         final String access_token = sharedpreferences.getString("access_token", "");
 
+        // Initialize Google Map and display Google Map
         mMapView = (MapView) view.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
@@ -70,14 +88,68 @@ public class MapFragment extends Fragment {
                 //googleMap.setMyLocationEnabled(true);
 
                 // For dropping a marker at a point on the Map
-                googleMap.addMarker(new MarkerOptions().position(new LatLng(-6.173460, 106.816920)).title("Central Jakarta").snippet("Melawat Tugu Negara Indonesia"));
-                googleMap.addMarker(new MarkerOptions().position(new LatLng(-6.129188, 106.833305)).title("Kota Jkt Utara").snippet("Enjoy Dunia Fantasi"));
+                // get list of coordinates and details
+                // get UI elements
+                final FrameLayout loadingFrame = (FrameLayout) view.findViewById(R.id.loadingFrame);
 
-                // initial lat lng for zoom
-                LatLng firstCoor = new LatLng(-6.173460, 106.816920);
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(firstCoor).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                // show loading spinner
+                loadingFrame.setVisibility(View.VISIBLE);
+
+                // Request a string response from the provided URL.
+                JsonArrayRequest coordinatesListRequest = new JsonArrayRequest(Request.Method.GET, AppHelper.baseurl + "/api/getLatLng/"+itinerary_id, null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                for(int i=0;i<response.length();i++)
+                                {
+                                    try {
+                                        JSONObject data = response.getJSONObject(i);
+                                        String place_name = data.getString("place_name");
+                                        String activity = data.getString("activity");
+                                        Double lat = Double.parseDouble(data.getString("lat"));
+                                        Double lng = Double.parseDouble(data.getString("lng"));
+
+                                        googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(place_name).snippet(activity));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                loadingFrame.setVisibility(View.GONE);
+
+                                if(response.length() > 0) {
+                                    try {
+                                        JSONObject initialData = response.getJSONObject(0);
+                                        // initial lat lng for zoom
+                                        LatLng firstCoor = new LatLng(Double.parseDouble(initialData.getString("lat")), Double.parseDouble(initialData.getString("lng")));
+                                        // For zooming automatically to the location of the marker
+                                        CameraPosition cameraPosition = new CameraPosition.Builder().target(firstCoor).zoom(12).build();
+                                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if(isAdded()) {
+                            Toast.makeText(getActivity().getApplicationContext(), "Load coordinates Failed! Please check your connection.", Toast.LENGTH_SHORT).show();
+                        }
+                        loadingFrame.setVisibility(View.GONE);
+                    }
+                })
+                {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("Authorization", "Bearer "+access_token);
+
+                        return params;
+                    }
+                };
+
+                // Add the request to the VolleySingleton.
+                VolleySingleton.getInstance(getActivity().getBaseContext()).addToRequestQueue(coordinatesListRequest);
             }
         });
 
@@ -107,5 +179,4 @@ public class MapFragment extends Fragment {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
-
 }
