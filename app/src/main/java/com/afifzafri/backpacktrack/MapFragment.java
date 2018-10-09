ea884a83c81ce5a8b085109c7ff70dcbadb7ad9e
Afif Zafri <zafri_21596@yahoo.com>
2018-10-10 03:04:37 +0800

    test output map with markers

--- app/src/main/java/com/afifzafri/backpacktrack/MapFragment.java
+++ app/src/main/java/com/afifzafri/backpacktrack/MapFragment.java
@@ -1,19 +1,35 @@
 package com.afifzafri.backpacktrack;
 
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap googleMap;
 
 
     public MapFragment() {
@@ -34,7 +50,62 @@ public class MapFragment extends Fragment {
         final SharedPreferences sharedpreferences = getActivity().getSharedPreferences("logindata", Context.MODE_PRIVATE);
         final String access_token = sharedpreferences.getString("access_token", "");
 
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                //googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(-6.173460, 106.816920)).title("Central Jakarta").snippet("Melawat Tugu Negara Indonesia"));
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(-6.129188, 106.833305)).title("Kota Jkt Utara").snippet("Enjoy Dunia Fantasi"));
+
+                // initial lat lng for zoom
+                LatLng firstCoor = new LatLng(-6.173460, 106.816920);
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(firstCoor).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 
 }
