b1493f2355dea39e575f51c17f26791bdc480c20
Afif Zafri <zafri_21596@yahoo.com>
2018-10-14 22:22:41 +0800

    added loading animation frame

--- app/src/main/java/com/afifzafri/backpacktrack/CommentsFragment.java
+++ app/src/main/java/com/afifzafri/backpacktrack/CommentsFragment.java
@@ -1,11 +1,16 @@
 package com.afifzafri.backpacktrack;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.github.mikephil.charting.charts.PieChart;
 
 
 /**
@@ -23,7 +28,19 @@ public class CommentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comments, container, false);
+        View view = inflater.inflate(R.layout.fragment_comments, container, false);
+
+        // get itinerary id
+        final String itinerary_id = getArguments().getString("itinerary_id");
+
+        // read from SharedPreferences
+        final SharedPreferences sharedpreferences = getActivity().getSharedPreferences("logindata", Context.MODE_PRIVATE);
+        final String access_token = sharedpreferences.getString("access_token", "");
+
+        // get UI elements
+        final FrameLayout loadingFrame = (FrameLayout) view.findViewById(R.id.loadingFrame);
+
+        return view;
     }
 
 }
