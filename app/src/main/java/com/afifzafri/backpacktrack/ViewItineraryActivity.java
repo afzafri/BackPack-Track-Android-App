67ef2b5b2ccf161986934442b518aba8a61be8e1
Afif Zafri <zafri_21596@yahoo.com>
2018-10-22 22:06:03 +0800

    enable deeplink, and get data send from deeplink. For auto launch apps from the web

--- app/src/main/java/com/afifzafri/backpacktrack/ViewItineraryActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/ViewItineraryActivity.java
@@ -1,9 +1,11 @@
 package com.afifzafri.backpacktrack;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class ViewItineraryActivity extends AppCompatActivity {
@@ -19,13 +21,28 @@ public class ViewItineraryActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back navigation
         getSupportActionBar().setElevation(0); // remove shadow
 
-        // get data pass through intent
-        Bundle extras = getIntent().getExtras();
-        final String itinerary_id = extras.getString("itinerary_id");
-        String itinerary_title = extras.getString("itinerary_title");
-        String itinerary_user_id = extras.getString("itinerary_user_id");
+        // declare variables
+        String itinerary_id = null;
+        String itinerary_title = null;
+        String itinerary_user_id = null;
         Boolean viewComment = false;
+
+        // If activity start from other intent get data pass through intent
+        Bundle extras = getIntent().getExtras();
+        itinerary_id = extras.getString("itinerary_id");
+        itinerary_title = extras.getString("itinerary_title");
+        itinerary_user_id = extras.getString("itinerary_user_id");
         viewComment = extras.getBoolean("viewComment");
+
+        // If activity launched from web, get data from deeplink
+        Uri deepLink = this.getIntent().getData();
+        if (deepLink != null && deepLink.isHierarchical()) {
+            itinerary_id = deepLink.getQueryParameter("itinerary_id");
+            itinerary_title = deepLink.getQueryParameter("itinerary_title");
+            itinerary_user_id = deepLink.getQueryParameter("itinerary_user_id");
+        }
+
+        // set activity action bar title
         setTitle(itinerary_title);
 
         // get the reference of ViewPager and TabLayout
