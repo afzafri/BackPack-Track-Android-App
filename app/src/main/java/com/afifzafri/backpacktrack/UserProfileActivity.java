93eed9299f4b6b96e840dae9ca6dd5159502841f
Afif Zafri <zafri_21596@yahoo.com>
2018-10-25 01:40:53 +0800

    created public user profile

--- app/src/main/java/com/afifzafri/backpacktrack/UserProfileActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/UserProfileActivity.java
@@ -1,8 +1,29 @@
 package com.afifzafri.backpacktrack;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -12,6 +33,114 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back navigation
+
+        // get data pass through intent
+        Bundle extras = getIntent().getExtras();
+        final String user_id = extras.getString("user_id");
+
+        // read from SharedPreferences
+        final SharedPreferences sharedpreferences = getSharedPreferences("logindata", Context.MODE_PRIVATE);
+        final String access_token = sharedpreferences.getString("access_token", "");
+
+        // get UI elements
+        final TextView textName = (TextView) findViewById(R.id.textName);
+        final TextView textUsername = (TextView) findViewById(R.id.textUsername);
+        final TextView textBio = (TextView) findViewById(R.id.textBio);
+        final TextView textCountry = (TextView) findViewById(R.id.textCountry);
+        final TextView textEmail = (TextView) findViewById(R.id.textEmail);
+        final TextView textWebsite = (TextView) findViewById(R.id.textWebsite);
+        final TextView textTotal = (TextView) findViewById(R.id.textTotal);
+        final ImageView avatar_pic = (ImageView) findViewById(R.id.avatar_pic);
+        final Button itineraryBtn = (Button) findViewById(R.id.itineraryBtn);
+        final FrameLayout loadingFrame = (FrameLayout) findViewById(R.id.loadingFrame);
+
+        // show loading spinner
+        loadingFrame.setVisibility(View.VISIBLE);
+
+        // ----- Fetch article data and display -----
+        // Request a string response from the provided URL.
+        JsonObjectRequest userRequest = new JsonObjectRequest(Request.Method.GET, AppHelper.baseurl + "/api/getUserData/" + user_id, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+                            // parse JSON response
+                            int id = response.getInt("id");
+                            String name = response.getString("name");
+                            String username = response.getString("username");
+                            String phone = response.getString("phone");
+                            String address = response.getString("address");
+                            String bio = response.getString("bio");
+                            String website = response.getString("website");
+                            String email = response.getString("email");
+                            String totalitineraries = response.getString("totalitineraries");
+                            String avatar_url = response.getString("avatar_url");
+                            JSONObject country = response.getJSONObject("country");
+                            String country_name = country.getString("name");
+
+                            // set values to the elements
+                            textName.setText(name);
+                            textUsername.setText("@"+username);
+                            textBio.setText(bio);
+                            textWebsite.setText(website.replaceFirst("^(http[s]?://www\\.|http[s]?://|www\\.)",""));
+                            textEmail.setText(email);
+                            textCountry.setText(country_name);
+                            textTotal.setText(totalitineraries);
+
+                            // check if bio and website not available (because optional), hide the widgets
+                            if(bio == null || bio.isEmpty() || bio == "null") {
+                                textBio.setVisibility(View.GONE);
+                            }
+                            if(website == null || website.isEmpty() || website == "null") {
+                                textWebsite.setVisibility(View.GONE);
+                            }
+
+                            // set avatar image using Picasso library
+                            if(avatar_url != null && !avatar_url.isEmpty() && avatar_url != "null") {
+                                // check if activity have been attach to the fragment
+                                Picasso.get()
+                                        .load(avatar_url)
+                                        .transform(new BorderedCircleTransformation(getResources().getColor(R.color.colorPrimary),5))
+                                        .into(avatar_pic);
+                                avatar_pic.setTag(avatar_url); // store url into tag, used for retrieve later
+                            } else {
+                                Picasso.get()
+                                        .load(R.drawable.avatar)
+                                        .transform(new BorderedCircleTransformation(getResources().getColor(R.color.colorPrimary),5))
+                                        .into(avatar_pic);
+                            }
+
+                            Toast.makeText(getApplicationContext(), "Profile data loaded!", Toast.LENGTH_SHORT).show();
+                            loadingFrame.setVisibility(View.GONE); // hide loading spinner
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            loadingFrame.setVisibility(View.GONE);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Profile not loaded! Please check your connection.", Toast.LENGTH_SHORT).show();
+                loadingFrame.setVisibility(View.GONE);
+            }
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer "+access_token);
+
+                return params;
+            }
+        };
+
+        // Add the request to the VolleySingleton.
+        VolleySingleton.getInstance(getBaseContext()).addToRequestQueue(userRequest);
+
     }
 
     // override default back navigation action
