9bed802f8061566dbbec977c35905e644c0bc3c3
Afif Zafri <zafri_21596@yahoo.com>
2018-10-26 16:12:03 +0800

    added tab view and assign pager adapter to view user itineraries

--- app/src/main/java/com/afifzafri/backpacktrack/UserItinerariesActivity.java
+++ app/src/main/java/com/afifzafri/backpacktrack/UserItinerariesActivity.java
@@ -1,11 +1,16 @@
 package com.afifzafri.backpacktrack;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class UserItinerariesActivity extends AppCompatActivity {
+
+    ViewPager simpleViewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,7 @@ public class UserItinerariesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_itineraries);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back navigation
+        getSupportActionBar().setElevation(0); // remove shadow
 
         // get data pass through intent
         Bundle extras = getIntent().getExtras();
@@ -21,6 +27,45 @@ public class UserItinerariesActivity extends AppCompatActivity {
 
         // set activity title
         setTitle(user_name + "'s itineraries");
+
+        // get the reference of ViewPager and TabLayout
+        simpleViewPager = (ViewPager) findViewById(R.id.simpleViewPager);
+        tabLayout = (TabLayout) findViewById(R.id.simpleTabLayout);
+
+        // Create a new tabs
+        TabLayout.Tab firstTab = tabLayout.newTab();
+        firstTab.setText("New"); // set the Text for the first Tab
+        tabLayout.addTab(firstTab); // add  the tab at in the TabLayout
+
+        TabLayout.Tab secondTab = tabLayout.newTab();
+        secondTab.setText("Top");
+        tabLayout.addTab(secondTab);
+
+        TabLayout.Tab thirdTab = tabLayout.newTab();
+        thirdTab.setText("Trending");
+        tabLayout.addTab(thirdTab);
+
+        ItinerariesPagerAdapter adapter = new ItinerariesPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount(), "user", user_id);
+        simpleViewPager.setAdapter(adapter);
+        // addOnPageChangeListener event change the tab on slide
+        simpleViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                simpleViewPager.setCurrentItem(tab.getPosition());
+                //Log.i("TAG", "onTabSelected: " + tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //Log.i("TAG", "onTabUnselected: " + tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                //Log.i("TAG", "onTabReselected: " + tab.getPosition());
+            }
+        });
     }
 
     // override default back navigation action
